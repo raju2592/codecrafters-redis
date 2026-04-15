1d045b05fa1f07428b5b09bfb2c84e62cea1fd68
Raju Ahmed <rajuahmedcse@gmail.com>
2026-04-15 07:01:28 +0600

    deliver message

--- app/commands/publish.go
+++ app/commands/publish.go
@@ -1,13 +1,29 @@
 package commands
 
 import (
+	"fmt"
+
 	"github.com/codecrafters-io/redis-starter-go/app/resp"
 )
 
 func PublishHandler(input []resp.RespValue, conn *ConnMeta) []byte {
 	channel := resp.GetStringValue(input[1])
+	message := resp.GetStringValue(input[2])
 
 	subs, ok := subscribers.Get(channel)
+
+	data := resp.SerializeArray([]resp.RespValue{
+		{ Ttype: resp.RespBulkString, Value: []byte("message") },
+		{ Ttype: resp.RespBulkString, Value: []byte(channel) },
+		{ Ttype: resp.RespBulkString, Value: []byte(message) },
+	})
+
+	for _, sub := range subs {
+		_, err := sub.Write(data)
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+		}
+	}
 
 	var sc int64 = 0;
 	if ok {
