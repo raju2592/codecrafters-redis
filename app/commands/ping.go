6956a3c9c8564de979b603709f71f88d9fe963b8
Raju Ahmed <rajuahmedcse@gmail.com>
2026-04-15 06:33:58 +0600

    implement subscribed ping

--- app/commands/ping.go
+++ app/commands/ping.go
@@ -5,5 +5,11 @@ import (
 )
 
 func PingHandler(input []resp.RespValue, conn *ConnMeta) []byte {
+	if (conn.mode == SubscribedMode) {
+		return resp.SerializeArray([]resp.RespValue{
+			{ Ttype: resp.RespBulkString, Value: []byte("pong") },
+			{ Ttype: resp.RespBulkString, Value: []byte("") },
+		})
+	}
 	return []byte("+PONG\r\n")
 }
